a5933e81948a597abdc0914f0f0aee95a1d034f9
Tihitna-22 <tihitna222sisay@gmail.com>
2022-12-21 11:14:22 +0300

    add dependancy

--- src/test.jsx
+++ src/test.jsx
@@ -72,8 +72,9 @@ const RenderInnerTree = (props) => {
   return (
     <>
     <TreeNode
-    label={<>
-      <div class=" rounded-lg shadow-lg bg-white w-2/5 text-center">
+    label={
+    <>
+      <div class=" rounded-lg shadow-lg bg-white max-w-sm  text-center">
         <div class="py-3 px-6 border-b border-gray-300 flex justify-between ">
           <div>  {props.item.label}</div>
           <div className="self-center">  
@@ -134,15 +135,77 @@ const RenderInnerTree = (props) => {
    
         {collapse && props.item.children && props.item.children?.map((ch)=>
         <>
+        {
+          ch.children ? <RenderInnerTree  item={ch}/> : <TreeNode label={
+            <div class=" rounded-lg shadow-lg bg-white max-w-sm  text-center">
+            <div class="py-3 px-6 border-b border-gray-300 flex justify-between ">
+              <div>  {props.item.label}</div>
+              <div className="self-center">  
+               <Menu offset={10} arrowPosition="center" transition="rotate-right" position="right-start">
+          <Menu.Target>
          
-         <RenderInnerTree  item={ch}/>
+               <button  class="relative z-10 block p-2 text-gray-700 bg-white border border-transparent rounded-md dark:text-white focus:border-blue-500 focus:ring-opacity-40 dark:focus:ring-opacity-40 focus:ring-blue-300 dark:focus:ring-blue-400 focus:ring dark:bg-gray-800 focus:outline-none">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="#EAB308">
+                <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
+            </svg>
+        </button>
+          </Menu.Target>
+    
+          <Menu.Dropdown>
+            <Menu.Item  icon={<Edit
+        size={25}
+        strokeWidth={1.5}
+        color={'#EAB308'}
+      />} onClick={()=>navigate(`/editUser/${props.item.id}`)}>
+              Edit
+            </Menu.Item>
+            <Menu.Item  icon={<Trash
+        size={25}
+        strokeWidth={1.5}
+        color={'#EAB308'}
+      />} onClick={() =>
+        handleDelet(props.item.id)
+        }>
+              Delete
+            </Menu.Item>
+    
+      
+          </Menu.Dropdown>
+           </Menu></div>
+          
+         
+            </div>
+    
+            <div class="p-6">
+            <h5 class="text-gray-900 text-xl font-medium mb-2"><title></title></h5>
+            <p class="text-gray-700 text-base mb-4">
+              discription
+            </p>
+            { props.item.children &&
+           ( <div className="flex justify-center ">
+              <button onClick={() => setCollaps((o) => !o)} class="relative z-10 block rounded-md bg-white p-2 focus:outline-none">
+              <svg class="h-5 w-5 text-gray-800" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#EAB308">
+              <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+              </svg>
+              </button>
+            </div>)}
+            </div>
+          </div>
+          }
+          
+          
+          
+          />
+        }
+    
+         
         {/* <AddNode parentId={ch.parentId}/> */}
         {/* {console.log(ch.parentId)} */}
-       
+      
         </>
        
         )}
-        {collapse && props.item.children ? <AddNode parentId={props.item.children[0].parentId}/>: ""}
+        {collapse && props.item.children && <AddNode parentId={props.item.children[0].parentId}/>}
          
   </TreeNode >
   
